0833a143f9a93904b017e4177bb6af9c2d630fb2
Joao <joaoup20@gmail.com>
2023-05-12 08:27:05 -0300

    js

---
+++ js/config.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded',function(){
+    main = document.querySelector('main')
+    input_foto_perfil = main.querySelector('input[type="file"]')
+    imagem = input_foto_perfil.value
+})
